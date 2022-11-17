16ec51aa057e2314b1be05fa2f180a7fbad9d784
izabellagrigoryan <bellagrigoryanmail@gmail.com>
2022-11-17 10:45:50 +0400

    Push Calculator Console Application

---
+++ source/repos/ConsoleAppCalculator/Program.cs
@@ -0,0 +1,72 @@
+﻿
+using Calculator;
+
+namespace ConsoleAppCalculator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                try
+                {
+                    float firstNumber = 0, secondNumber = 0;
+                    string operation;
+                    Console.WriteLine("Please enter first number");
+                    firstNumber = (float)Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Please enter second number");
+                    secondNumber = (float)Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Please enter type of operation");
+                    operation = Console.ReadLine();
+
+                    SimpleCalculator simpCalc = new SimpleCalculator(firstNumber, secondNumber);
+
+                    switch (operation)
+                    {
+                        case "+":
+                            {
+                                Console.WriteLine(simpCalc.Summarize());
+                                break;
+                            }
+                        case "-":
+                            {
+                                Console.WriteLine(simpCalc.Deduct());
+                                break;
+                            }
+                        case "*":
+                            {
+                                Console.WriteLine(simpCalc.Multiply());
+                                break;
+                            }
+                        case "/":
+                            {
+                                Console.WriteLine(simpCalc.Divide());
+                                break;
+                            }
+                        case "%":
+                            {
+                                Console.WriteLine(simpCalc.Percent());
+                                break;
+                            }
+                    }
+                }
+                catch (FormatException)
+                { 
+                    Console.WriteLine("Can't convert a string to number. Please enter number");
+                }
+                catch (DivideByZeroException)
+                {
+                    Console.WriteLine("The second number mustn't be zero");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+
+
+
+        }
+    }
+}
