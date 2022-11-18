4d025618d9ff200176cf6091bc2572781177a8c6
izabellagrigoryan <bellagrigoryanmail@gmail.com>
2022-11-18 20:32:17 +0400

    Calculator class library 20:32

---
+++ Calculator/SimpleCalculator.cs
@@ -0,0 +1,31 @@
+﻿namespace Calculator
+{
+    public class SimpleCalculator
+    {
+        public float FirstNumber { get; set; }
+        public float SecondNumber { get; set; }
+
+
+        public SimpleCalculator(float firstNumber, float secondNumber)
+        {
+            FirstNumber = firstNumber;
+            SecondNumber = secondNumber;
+        }
+
+
+        public float Summarize()
+        { return FirstNumber + SecondNumber; }
+
+        public float Deduct()
+        { return FirstNumber - SecondNumber; }
+
+        public float Multiply()
+        { return FirstNumber * SecondNumber; }
+
+        public float Divide()
+        { return FirstNumber / SecondNumber; }
+
+        public float Percent()
+        { return FirstNumber / 100 * SecondNumber; }
+    }
+}
